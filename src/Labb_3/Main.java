53bdfe44aa8daeab2894281d6c701db2f3090844
Andreas Berg <andreas.berg2@student.kyh.se>
2020-11-17 12:37:04 +0100

    progress

--- src/Labb_3/Main.java
+++ src/Labb_3/Main.java
@@ -2,6 +2,18 @@ package Labb_3;
 
 public class Main {
 
+    private static void print(int[][] board){
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                System.out.print(board[x][y] + " ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
     private static boolean possible(int[][] board, int x, int y, int value){
         //kontrollera rad och kolumn
         for(int i = 0; i < 9; i++){
@@ -10,8 +22,8 @@ public class Main {
             }
         }
         //kontrollera rutan
-        int x0 = (int)Math.floor(x/3) * 3;
-        int y0 = (int)Math.floor(y/3) * 3;
+        int x0 = (x/3) * 3;
+        int y0 = (y/3) * 3;
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 if (board[y0+i][x0+j] == value){
@@ -23,6 +35,22 @@ public class Main {
     }
 
     private static void solve(int[][] board){
+        for(int i= 0; i < 9; i++) {
+            for (int j= 0; j < 9; j++){
+                if (board [i][j] == 0){
+                    for(int k= 1; k < 10; k++){
+                        if (possible(board, i, j, k)){
+                            board [i][j] = k;
+                            solve(board);
+                            board[i][j] = 0;
+
+                        }
+                    }
+                }print(board);
+            }
+        }
+
+
         //Loopa igenom alla rader och kolumner
         //  Kolla om det är en nolla på aktuell pos
         //      Loopa igenom alla värden från 1 till 9
@@ -36,17 +64,32 @@ public class Main {
 
     public static void main(String[] args) {
         int [][] board = {{0, 0, 0, 8, 0, 4, 9, 3, 7},
-                {0, 7, 4, 1, 0, 0, 0, 8, 0},
-                {8, 3, 2, 0, 0, 0, 4, 0, 0},
-                {2, 0, 5, 3, 0, 0, 7, 4, 0},
-                {0, 0, 0, 0, 0, 0, 0, 1, 6},
-                {1, 4, 3, 0, 0, 0, 2, 0, 0},
-                {0, 0, 7, 0, 9, 0, 6, 0, 0},
-                {0, 2, 1, 7, 5, 6, 8, 9, 0},
-                {6, 5, 9, 2, 3, 0, 0, 7, 4}};
-        System.out.println(possible(board, 4, 4, 2));
-        System.out.println(possible(board, 4, 4, 1));
-        System.out.println(possible(board, 5, 6, 3));
-        System.out.println(possible(board, 0, 0, 5));
+                          {0, 7, 4, 1, 0, 0, 0, 8, 0},
+                          {8, 3, 2, 0, 0, 0, 4, 0, 0},
+                          {2, 0, 5, 3, 0, 0, 7, 4, 0},
+                          {0, 0, 0, 0, 0, 0, 0, 1, 6},
+                          {1, 4, 3, 0, 0, 0, 2, 0, 0},
+                          {0, 0, 7, 0, 9, 0, 6, 0, 0},
+                          {0, 2, 1, 7, 5, 6, 8, 9, 0},
+                          {6, 5, 9, 2, 3, 0, 0, 7, 4}};
+
+
+        solve(board);
+
+
+
+
+        //System.out.println(board [0][0]);
+
+
+
+
+
+
+
+        /*System.out.println("Should be " + possible(board, 4, 4, 2));
+        System.out.println("Should be " + possible(board, 4, 4, 1));
+        System.out.println("Should be " + possible(board, 5, 6, 3));
+        System.out.println("Should be " + possible(board, 0, 0, 5));*/
     }
 }
